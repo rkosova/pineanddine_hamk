77a8cad440ce44c1da7dfbb2c5ff28cb60b008e6
nazaninfallah <fallahnaznin@gmail.com>
2022-03-04 10:07:19 -0800

    .

---
+++ header_footer/footer.php
@@ -0,0 +1,32 @@
+
+<div class="footer">
+	<div class="footer-text" id="location">
+		<p>LOCATION
+		<br>
+		<br>Finland- Helsinki- 
+		<br> Aleksanterinkatu 12234</p>	
+	</div>
+
+	<div class="footer-text" id="hours">
+		<p>HOURS
+		<br>
+		<br>MONDAY-THURSDAY 
+		<br>17.30-11.30
+		<br>FRIDAY-SATURDAY
+		<br>17.00-01.00</p>	
+	</div>
+
+	<div class="footer-text" id="contact">
+		<p>CONTACT US
+		<br>
+		<br>+3587235482921
+		<br>+3587263273271
+		<br>
+		<br>pineanddine@pdgroup.fi</p>	
+	</div>
+
+	<div class="footer-text" id="socials">
+		<img src="./images/fbwhite.png">
+		<img src="./images/igwhite.png">
+	</div>
+</div>
